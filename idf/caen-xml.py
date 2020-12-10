7b0077eac5642165f9067a92f1ecb057f0ac4e68
jorden95 <michiel.jordens@imec.be>
2020-12-10 16:40:02 +0100

    Added example as is from the webpage

--- idf/caen-xml.py
+++
@@ -1,6 +0,0 @@
-#!/bin/python
-import xmlschema
-
-if __name__ == "__main__":
-    idf = xmlschema.XMLSchema("idfv1_02.xsd")
-    print(idf.is_valid("IAEA25_NDF.xml"))
---
+++ idf/validate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xmllint --schema idfv1_02.xsd IAEA25_NDF.xml --noout
+
+
