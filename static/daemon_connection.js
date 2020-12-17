00a6601fc604d03c8657ebc85d6264d1fef350c9
micjo <5049483+micjo@users.noreply.github.com>
2020-12-17 17:23:44 +0100

    Updated layout for minimal interface and added alive check

--- static/daemon_connection.js
+++ static/daemon_connection.js
@@ -1,4 +1,4 @@
-export {aml_xy, aml_det, aml_phi, caen_host_1}
+export {aml_xy_act, aml_det_act, aml_phi_act, caen_host_1_act}
 
 let aml_host = 'http://localhost'
 let caen_host = 'http://localhost'
@@ -7,3 +7,8 @@ let aml_xy = aml_host + ":22000"
 let aml_det = aml_host + ":22001"
 let aml_phi = aml_host + ":22002"
 let caen_host_1 = caen_host + "22003"
+
+let aml_xy_act = aml_xy + "/actuals"
+let aml_det_act = aml_det + "/actuals"
+let aml_phi_act = aml_phi + "/actuals"
+let caen_host_1_act = caen_host_1 + "22003" + "/actuals"
