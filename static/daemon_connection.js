9cd419c16c6c1255b12f116681434fedb7517489
micjo <5049483+micjo@users.noreply.github.com>
2020-12-17 10:58:00 +0100

    made modules for packages and generic calls. Updated layout for minimal interface

---
+++ static/daemon_connection.js
@@ -0,0 +1,9 @@
+export {aml_xy, aml_det, aml_phi, caen_host_1}
+
+let aml_host = 'http://localhost'
+let caen_host = 'http://localhost'
+
+let aml_xy = aml_host + ":22000"
+let aml_det = aml_host + ":22001"
+let aml_phi = aml_host + ":22002"
+let caen_host_1 = caen_host + "22003"
---
+++ static/min_itf.js
@@ -0,0 +1,3 @@
+import * as con from './daemon_connection.js'
+
+document.getElementById("xy_con").innerText = con.aml_host;
