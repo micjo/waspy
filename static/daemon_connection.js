c5c8949f1bc9a274d55d5bf982e5a829f2e55d46
micjo <5049483+micjo@users.noreply.github.com>
2021-01-12 11:56:49 +0100

    streamlined calls with element ids

--- static/daemon_connection.js
+++ static/daemon_connection.js
@@ -1,32 +1,29 @@
-export {sendRequest, getUniqueIdentifier, collapsableError, collapsableSucess, collapsableNotify}
-export {getEl}
-export {motors,dataAcq}
-
-let motors = {
-    xyUrl : 'http://localhost:22000',
-    detTheta : {
-        responseUrl : 'http://localhost:22001/actuals',
-        requestUrl : 'http://localhost:22001/engine'
-    },
-    phiZeta : {
-        responseUrl : 'http://localhost:22002/actuals',
-        requestUrl : 'http://localhost:22002/engine'
-    }
+export {
+    collapsableError,
+    collapsableNotify,
+    collapsableSucess,
+    getUniqueIdentifier,
+    sendRequest
 };
 
-let dataAcq = {
-    caen : {
-        responseUrl : 'http://localhost:22003/actuals',
-        requestUrl : 'http://localhost:22003/engine'
-    }
-}
+export {getEl};
+export {dataAcq, motors};
+export {getAmlActuals};
 
-function postData(host, textBody)
-{
+let motors = {
+    xyUrl: 'http://localhost:22000',
+    detThetaUrl: 'http://localhost:22001',
+    phiZetaUrl: 'http://localhost:22002'
+};
+
+let dataAcq =
+    {caenUrl: 'http://localhost:22003/actuals'}
+
+function postData(host, textBody) {
     fetch(host, {
         method: 'POST',
         headers: {
-          'Content-Type': 'text/plain',
+            'Content-Type': 'text/plain',
         },
         body: textBody
     });
@@ -36,42 +33,96 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function updateSpinnerVisbility(elementId, data) {
+    if (data['status'] === 'Processing') {
+        getEl(elementId).style.display = 'block';
+    } else if (data['status'] === 'Done') {
+        getEl(elementId).style.display = 'none';
+    }
+}
 
-function getCompletionTime(url, requestId, retryLimit, retryCount) {
-    return fetch(url)
-        .then (response => response.json())
-        .then (data => {
-            console.log(data);
-            if ( data["request_id"] === requestId) {
-                console.log("match");
-                console.log(data);
-                return data["expiry_date"];
+function setConnected(id, connected) {
+    if (connected) {
+        document.getElementById(id).innerText = 'Connected';
+        document.getElementById(id).setAttribute(
+            'class', 'badge badge-success');
+    } else {
+        document.getElementById(id).innerText = 'Disconnected';
+        document.getElementById(id).setAttribute('class', 'badge badge-danger');
+    }
+}
+
+function getCompletionTimeForAmlRequest(url, elementId, requestId, retryLimit, retryCount) {
+    return getAmlActuals(url, elementId).then(data => {
+        if (data['request_id'] === requestId) {
+            return data['expiry_date'];
+        } else if (retryCount < retryLimit) {
+            return delay(100).then(
+                () => getCompletionTimeForAmlRequest(
+                    url, elementId, requestId, retryLimit, retryCount + 1));
+        }
+    });
+}
+
+function getAmlActuals(url, elementId) {
+    let connectionId = elementId + '_connect_status';
+    let errorId = elementId + '_error_status';
+    let busyId = elementId + '_busy_status';
+    let requestId = elementId + '_request_id';
+    let firstMotorId = elementId + '_first';
+    let secondMotorId = elementId + '_second';
+
+    return fetch(url + '/actuals')
+        .then(response => {
+            setConnected(connectionId, true);
+            return response.json();
+        })
+        .then(data => {
+            getEl(firstMotorId).innerText =
+                data['motor1']['position_real_world'];
+            getEl(secondMotorId).innerText =
+                data['motor2']['position_real_world'];
+            getEl(requestId).innerText = data['request_id'];
+
+            if (data['error_status'] !== 'Success') {
+                collapsableError(errorId, data['error_status']);
+            } else {
+                getEl(errorId).innerHTML = '';
             }
-            else if (retryCount < retryLimit) {
-                return delay(100).then(() => getCompletionTime(url, requestId, retryLimit, retryCount+1));
-            }
+            updateSpinnerVisbility(busyId, data);
+            return data;
+        })
+        .catch(error => {
+            setConnected(connectionId, false);
+            collapsableError(errorId, error);
+            getEl(firstMotorId).innerText = '-';
+            getEl(secondMotorId).innerText = '-';
         });
 }
 
-async function sendRequest(url, requestId, request, statusId) {
-    postData(url+"/engine", request);
-    let expiry_date = await getCompletionTime(url+"/actuals", requestId, 10, 0);
-    if (expiry_date === undefined) {
-        collapsableError(statusId, "Failed to send request");
-    }
-    else {
-        collapsableSucess(statusId, "Request sent, should be done at: " + expiry_date);
+async function sendRequest(url, request, elementId) {
+    let requestId = getUniqueIdentifier();
+    let fullRequest = 'request_id=' + requestId + '\n';
+    fullRequest += request;
+
+    let requestStatusId = elementId + '_request_status';
+    postData(url + '/engine', fullRequest);
+    let expiry_date = await getCompletionTimeForAmlRequest(url, elementId, requestId, 10, 0);
+    if (expiry_date !== undefined) {
+        collapsableSucess( requestStatusId,
+            'Request sent with id: ' + requestId +
+            ', should be done at: ' + expiry_date);
     }
 }
 
 
 function getUniqueIdentifier() {
-    let timestamp = new Date(Date.now()).toLocaleTimeString("nl-BE");
-    return  timestamp;
+    let timestamp = new Date(Date.now()).toLocaleTimeString('nl-BE');
+    return timestamp;
 }
 
-function collapsableNotify(id,message) {
-    let notifyDiv = makeAlert("alert-secondary", message);
+function collapsableNotify(id, message) {
+    let notifyDiv = makeAlert('alert-secondary', message);
     document.getElementById(id).appendChild(notifyDiv);
     setTimeout(function() {
         document.getElementById(id).removeChild(notifyDiv);
@@ -79,14 +130,14 @@ function collapsableNotify(id,message) {
 }
 
 function collapsableError(id, message) {
-    getEl(id).innerHTML = "";
-    let errorDiv = makeAlert("alert-danger", message);
+    getEl(id).innerHTML = '';
+    let errorDiv = makeAlert('alert-danger', message);
     document.getElementById(id).appendChild(errorDiv);
 }
 
 function collapsableSucess(id, message) {
-    getEl(id).innerHTML = "";
-    let successDiv = makeAlert("alert-success", message);
+    getEl(id).innerHTML = '';
+    let successDiv = makeAlert('alert-success', message);
     document.getElementById(id).appendChild(successDiv);
     setTimeout(function() {
         document.getElementById(id).removeChild(successDiv);
@@ -95,15 +146,16 @@ function collapsableSucess(id, message) {
 
 function makeAlert(alertType, message) {
     let alertDiv = document.createElement('div');
-    alertDiv.classList.add("alert", alertType, "alert-dismissible", "top-margin");
-    alertDiv.setAttribute("role", "alert");
+    alertDiv.classList.add(
+        'alert', alertType, 'alert-dismissible', 'top-margin');
+    alertDiv.setAttribute('role', 'alert');
     alertDiv.innerText = message;
-    let closeDiv = document.createElement("button");
-    closeDiv.classList.add("close");
-    closeDiv.setAttribute("data-dismiss", "alert");
-    let spanDiv = document.createElement("span");
-    spanDiv.setAttribute("aria-hidden","true");
-    spanDiv.innerHTML = "&times";
+    let closeDiv = document.createElement('button');
+    closeDiv.classList.add('close');
+    closeDiv.setAttribute('data-dismiss', 'alert');
+    let spanDiv = document.createElement('span');
+    spanDiv.setAttribute('aria-hidden', 'true');
+    spanDiv.innerHTML = '&times';
     closeDiv.append(spanDiv);
     alertDiv.append(closeDiv);
 
