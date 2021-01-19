285cc34b9d0eeb11b01e448fce6f11bde36eb900
jorden95 <michiel.jordens@imec.be>
2021-01-19 11:52:51 +0100

    Cleaned up aml interface. added spinner boxes. added dashboard

--- static/aml.js
+++ static/aml.js
@@ -4,19 +4,26 @@ export {aml}
 
 
 class aml {
-    constructor(url, elementPrefix) {
+    constructor(url) {
         this.url = url;
-        this.elementPrefix = elementPrefix;
-        this.connectionId = elementPrefix + '_connect_status';
-        this.errorId = elementPrefix + '_error_status';
-        this.busyId = elementPrefix + '_busy_status';
-        this.requestId = elementPrefix + '_request_id';
-        this.firstMotorId = elementPrefix + '_first';
-        this.secondMotorId = elementPrefix + '_second';
-        this.requestStatus = elementPrefix + '_request_status';
+        this.connected = false;
+        this.error = '-';
+        this.busy = '-';
+        this.requestId = '-';
+        this.firstMotorPosition = '-';
+        this.secondMotorPosition = '-';
+        this.requestAcknowledge = true;
+        this.completionTime = '-';
+    }
 
-        this.firstMotorRequest = elementPrefix + '_first_request';
-        this.secondMotorRequest = elementPrefix + '_second_request';
+    removeData() {
+        this.error = '-';
+        this.busy = '-';
+        this.requestId = '-';
+        this.firstMotorPosition = '-';
+        this.secondMotorPosition = '-';
+        this.requestAcknowledge = true;
+        this.completionTIme = '-';
     }
 
     async init() {
@@ -26,98 +33,68 @@ class aml {
     async updateActuals(){
         return fetch(this.url + '/actuals')
             .then(response => {
-                con.setConnected(this.connectionId, true);
+                this.connected = true;
                 return response.json();
             })
-            .then(data => {
-                con.getEl(this.firstMotorId).innerText =
-                    data['motor1']['position_real_world'];
-                con.getEl(this.secondMotorId).innerText =
-                    data['motor2']['position_real_world'];
-                con.getEl(this.requestId).innerText = data['request_id'];
-
-                if (data['error_status'] !== 'Success') {
-                    con.collapsableError(this.errorId, data['error_status']);
-                } else {
-                    con.getEl(this.errorId).innerHTML = '';
-                }
-                updateSpinnerVisbility(this.busyId, data);
-                return data;
+            .catch( () => {
+                this.connected = false;
             })
-            .catch(error => {
-                con.setConnected(this.connectionId, false);
-                con.collapsableError(this.errorId, error);
-                con.getEl(this.firstMotorId).innerText = '-';
-                con.getEl(this.secondMotorId).innerText = '-';
-                con.getEl(this.requestId).innerText = '-';
+            .then(data => {
+                this.requestId = data['request_id'];
+                this.firstMotorPosition = data['motor1']['position_real_world'];
+                this.secondMotorPosition = data['motor2']['position_real_world'];
+                this.error = data['error_status'];
+                this.busy = data['status'];
+                this.completionTime = data['expiry_date'];
+            })
+            .catch(() => {
+                this.removeData();
             });
     }
 
+    async waitForCompleted(requestId, retryLimit, retryCount) {
+        return this.updateActuals().then( () => {
+            if (this.requestId === requestId) {
+                this.requestAcknowledge = true;
+                return;
+            }
+            else if (retryCount < retryLimit) {
+                console.log('wait for complete');
+                return con.delay(100)
+                    .then( () => this.waitForCompleted(requestId, retryLimit, retryCount + 1));
+            }
+            else {
+                this.requestAcknowledge = false;
+            }
+        })
+    }
+
     async sendRequest(request) {
+        this.requestAcknowledge = true;
         let requestId = con.getUniqueIdentifier();
         let fullRequest = 'request_id=' + requestId + '\n';
         fullRequest += request;
-        con.postData(this.url + '/engine', fullRequest);
-        let expiry_date = await this.getCompletionTime(requestId, 10, 0);
-        if (expiry_date !== undefined) {
-            con.collapsableSucess( this.requestStatus,
-                'Request sent with id: ' + requestId +
-                ', should be done at: ' + expiry_date);
+
+        await con.postData(this.url + '/engine', fullRequest);
+        await this.waitForCompleted(requestId, 30, 0);
+    }
+
+    async moveMotors(firstPosition, secondPosition) {
+        let request ='';
+        if (firstPosition && secondPosition) {
+            request += 'set_m1_target_position=' + firstPosition + '\n';
+            request += 'set_m2_target_position=' + secondPosition + '\n';
+            await this.sendRequest(request);
+        } else if (firstPosition) {
+            request += 'set_m1_target_position=' + firstPosition + '\n';
+            await this.sendRequest(request);
+        } else if (secondPosition) {
+            request += 'set_m2_target_position=' + secondPosition + '\n';
+            await this.sendRequest(request);
         }
     }
 
-    async getCompletionTime(requestId, retryLimit, retryCount) {
-        return this.updateActuals().then(data => {
-            if (data['request_id'] === requestId) {
-                return data['expiry_date'];
-            } else if (retryCount < retryLimit) {
-                return con.delay(100)
-                    .then(() => this.getCompletionTime(requestId, retryLimit, retryCount + 1));
-            }
-        });
-    }
-
-    submitMotors() {
-        let first_pos = con.getEl(this.firstMotorRequest).value;
-        let second_pos = con.getEl(this.secondMotorRequest).value;
-
-        let request = '';
-        if (first_pos && second_pos) {
-            request += 'set_m1_target_position=' + first_pos + '\n';
-            request += 'set_m2_target_position=' + second_pos + '\n';
-            this.sendRequest(request);
-        } else if (first_pos) {
-            request += 'set_m1_target_position=' + first_pos + '\n';
-            this.sendRequest(request);
-        } else if (second_pos) {
-            request += 'set_m2_target_position=' + second_pos + '\n';
-            this.sendRequest(request);
-        } else {
-            con.collapsableError(this.requestStatus, 'No input provided');
-        }
-    }
-
-    loadMotors() {
-        con.getEl(this.firstMotorRequest).value = '60';
-        con.getEl(this.secondMotorRequest).value = '10';
-        this.submitMotors();
-    }
-
-    unLoadMotors() {
-        con.getEl(this.firstMotorRequest).value = '0';
-        con.getEl(this.secondMotorRequest).value = '0';
-        this.submitMotors();
-    }
-
-    continueOnError() {
-        this.sendRequest('continue=true');
-    }
-}
-
-function updateSpinnerVisbility(elementId, data) {
-    if (data['status'] === 'Processing') {
-        con.getEl(elementId).style.display = 'block';
-    } else if (data['status'] === 'Done') {
-        con.getEl(elementId).style.display = 'none';
+    async continueOnError() {
+        await this.sendRequest('continue=true');
     }
 }
--- static/dashboard.js
+++ static/dashboard.js
@@ -1,11 +1,43 @@
 import * as aml from './aml.js'
 import * as caen from './caen.js'
+import * as con from './daemon_connection.js'
 
 let caen1 = new caen.caen('http://ubuntu-desktop:22123', 'caen');
 
-function refreshData() {
-    console.log("refreshing");
-    caen1.updateActuals();
+let amlXy = new aml.aml('http://localhost:22000');
+let amlDetTheta = new aml.aml('http://localhost:22001');
+let amlPhiZeta = new aml.aml('http://localhost:22002');
+
+function updateCaen() {
+    con.setConnected("caen_connect_status", caen1.connected);
+    con.setBadgeState("caen_error_status", caen1.error !== "Success");
+    con.getEl("caen_error_status").innerText = caen1.error;
+    con.getEl("caen_request_id").innerText = caen1.requestId;
+
+    if (caen1.acquiringData) {
+        con.getEl("caen_acquiring_data").innerText = "Acquiring active";
+    }
+    else {
+        con.getEl("caen_acquiring_data").innerText = "-";
+    }
+}
+
+function updateDaemon(prefix, activeDaemon) {
+    con.setConnected(prefix + "_connect_status", activeDaemon.connected);
+    con.setBadgeState(prefix + "_error_status", activeDaemon.error !== "Success");
+    con.getEl(prefix + "_error_status").innerText = activeDaemon.error;
+    con.getEl(prefix + "_request_id").innerText = activeDaemon.requestId;
+}
+
+async function refreshData() {
+    await caen1.updateActuals();
+    updateCaen();
+    await amlXy.updateActuals();
+    updateDaemon('aml_x_y', amlXy);
+    await amlDetTheta.updateActuals();
+    updateDaemon('aml_det_theta', amlDetTheta);
+    await amlPhiZeta.updateActuals();
+    updateDaemon('aml_phi_zeta', amlPhiZeta);
 }
 
 window.setInterval(function() {
