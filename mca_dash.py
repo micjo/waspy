d0fa544aaa303c6523ef62658ed10eb090617b33
mic-lab <jorden95@imec.be>
2021-01-22 12:09:26 +0100

    Fixed names for det theta and phi zeta

--- mca_dash.py
+++ mca_dash.py
@@ -15,15 +15,18 @@ def caen_max():
 
 @app.route("/aml_x_y")
 def aml_x_y():
-    return render_template("aml_max.html", prefix="aml_x_y", url='http://169.254.166.218:22000', load_first='72.50', load_second='61.7');
+    return render_template("aml_max.html", prefix="aml_x_y", url='http://169.254.166.218:22000', 
+    	load_first='72.50', load_second='61.7', first_name='X', second_name='Y');
 
 @app.route("/aml_det_theta")
 def aml_det_theta():
-    return render_template("aml_max.html", prefix="aml_det_theta", url='http://169.254.166.218:22001', load_first='170.00', load_second='-180.50');
+    return render_template("aml_max.html", prefix="aml_det_theta", url='http://169.254.166.218:22001', 
+    	load_first='170.00', load_second='-180.50', first_name='Det', second_name='Theta');
 
 @app.route("/aml_phi_zeta")
 def aml_phi_zeta():
-    return render_template("aml_max.html", prefix="aml_phi_zeta", url='http://169.254.166.218:22002', load_first='0.00', load_second='-1.00');
+    return render_template("aml_max.html", prefix="aml_phi_zeta", url='http://169.254.166.218:22002',
+    	load_first='0.00', load_second='-1.00', first_name='Phi', second_name='Zeta');
 
 @app.route("/favicon.ico")
 def favicon():
