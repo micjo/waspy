65d130e1c8fae590b61c29e835c3432a465684c2
micjo <5049483+micjo@users.noreply.github.com>
2021-04-27 17:46:50 +0200

    cleaned up unused modules. plotly is now available offline. More alignment between UI's

---
+++ static/daemon_connection.js
@@ -0,0 +1,234 @@
+export {getEl,getStatus};
+export {setBadgeState, setBadgeSuccess, setBadgeDanger, setText};
+export {sendRequestAndSpin, sendRequest, showFailureModal};
+export {configureUiCallBack};
+export {
+    sendInt,
+    sendFloat,
+    sendString,
+    sendARequest,
+    updateElement,
+    toggle,
+    refreshData
+};
+
+async function postData(host, textBody) {
+    fetch(host, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        body: textBody
+    });
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function setConnected(id, connected) {
+    if (connected) {
+        document.getElementById(id).innerText = 'Running';
+        document.getElementById(id).setAttribute(
+            'class', 'badge bg-success');
+    } else {
+        document.getElementById(id).innerText = 'Not running';
+        document.getElementById(id).setAttribute('class', 'badge bg-danger');
+    }
+}
+
+function setText(id, text) {
+    getEl(id).innerText = text;
+}
+
+function setBadgeState(id, errored) {
+    if (errored) {
+        getEl(id).setAttribute('class', 'badge bg-danger');
+    }
+    else {
+        getEl(id).setAttribute('class', 'badge bg-success');
+    }
+}
+
+function setBadgeSuccess(id) {
+    getEl(id).setAttribute('class', 'badge bg-success');
+}
+
+function setBadgeDanger(id) {
+    getEl(id).setAttribute('class', 'badge bg-danger');
+}
+
+function getUniqueIdentifier() {
+    let date = new Date();
+
+    // 0 + with slice(-2) ensures you have a leading 0 if needed
+    let year = date.getFullYear();
+    let month = ("0" + date.getMonth() + 1).slice(-2);
+    let day = ("0" + date.getDay() + 1).slice(-2);
+
+    let hours = ("0" + date.getHours()).slice(-2);
+    let minutes = ("0" + date.getMinutes()).slice(-2);
+    let seconds = ("0" + date.getSeconds()).slice(-2);
+
+    let timestamp = year + "." + month + "." + day + "__" + hours + ":" + minutes + "__" + seconds;
+
+    return timestamp;
+}
+
+
+function hide(element) {
+    getEl(element).setAttribute("style", "display:none");
+}
+
+function show(element) {
+    getEl(element).setAttribute("style", "display:inline-block");
+}
+
+function disable(buttonId){
+    getEl(buttonId).disabled=true;
+}
+
+function enable(buttonId){
+    getEl(buttonId).disabled=false;
+}
+
+function getEl(element) {
+    return document.getElementById(element);
+}
+
+
+async function getStatus(url) {
+    let dataFromDm;
+    await fetch(url)
+        .then(response => response.json())
+        .then(data => { dataFromDm = data; });
+    return dataFromDm;
+}
+
+async function waitForCompleted(url, requestId, retryLimit, retryCount) {
+    return fetch(url)
+        .then(response => {
+            if (response.status == 404) { throw 'Daemon not reachable.'; }
+            return response.json();
+        })
+        .then(data => {
+            if (data['request_id'] == requestId && data['request_finished'] == true) {
+                return data;
+            }
+            else {
+                if (retryCount < retryLimit) {
+                return delay(250)
+                .then( () => waitForCompleted(url, requestId, retryLimit, retryCount +1));
+                }
+            }
+        });
+}
+
+var uiCallBack;
+function configureUiCallBack(func) {
+    uiCallBack = func;
+}
+
+async function sendRequestAndSpin(url, prefix, id, request) {
+
+    let spinnerId = prefix + "_" + id + "_spinner";
+    let clickId = prefix + "_" + id + "_click";
+    show(spinnerId);
+    disable(clickId);
+
+    let data = await sendRequest(url, request);
+    uiCallBack(prefix, data);
+    hide(spinnerId);
+    enable(clickId);
+    return data
+}
+
+async function refreshData(url,prefix) {
+    let hwData = await getStatus(url)
+
+    if (hwData) {
+        setConnected(prefix + "_connect_status", true);
+        uiCallBack(prefix, hwData);
+    }
+    else {
+        setConnected(prefix + "_connect_status", false);
+    }
+
+}
+
+async function sendRequest(url, request) {
+    let requestId = { "request_id" : getUniqueIdentifier()}
+
+    let fullRequest = {
+        ...requestId,
+        ...request
+    };
+    await postData(url, JSON.stringify(fullRequest));
+    let data = await waitForCompleted(url, requestId["request_id"], 50, 0)
+        .catch(error => {
+            showFailureModal(error);
+        });
+    return data
+}
+
+
+function showFailureModal(text){
+    var myModal = new bootstrap.Modal(document.getElementById('modalFail'))
+    getEl("modalFailBody").innerText = text;
+    myModal.show()
+}
+
+
+function sendARequest(url, prefix, id ,request) {
+    let jsonRequest =  JSON.parse(request);
+    sendRequestAndSpin(url,prefix, id, jsonRequest);
+}
+
+function sendInt(url,prefix, id, requestKey) {
+    let value = parseInt(getEl(prefix + "_" +id + "_request").value);
+    if (!Number.isInteger(value)) {
+        showFailureModal('This is not a valid integer number.');
+        return;
+    }
+    let request = {};
+    request[requestKey] = value;
+    sendRequestAndSpin(url,prefix, id, request);
+}
+
+function sendString(url,prefix, id, requestKey) {
+    let value = getEl(prefix + "_" +id + "_request").value;
+    if (value === "Choose...") { return; }
+    let request = {};
+    request[requestKey] = value;
+    sendRequestAndSpin(url,prefix, id, request);
+}
+
+function sendFloat(url,prefix, id, requestKey) {
+    let value = parseFloat(getEl(prefix + "_" +id + "_request").value);
+    if (isNaN(value)) {
+        showFailureModal("This is not a valid floating point number");
+        return;
+    }
+    let request = {};
+    request[requestKey] = value;
+    sendRequestAndSpin(url,prefix, id, request);
+}
+
+async function toggle(url, prefix, id, requestKey) {
+    let value = getEl(prefix + "_" +id + "_click").checked;
+    let request = {};
+    request[requestKey] = value;
+    await sendRequestAndSpin(url,prefix, id, request);
+}
+
+function updateElement(id, value) {
+    if (value === undefined) {
+        getEl(id).innerText = "-";
+    }
+    else if (value === "") {
+        getEl(id).innerText = "-";
+    }
+    else {
+        getEl(id).innerText = value;
+    }
+}
--- static/max_aml.js
+++ static/max_aml.js
@@ -1,110 +1,66 @@
-import * as con from './controllers/daemon_connection.js'
+import * as con from './daemon_connection.js'
 
-export {refreshAllData, setMinMode};
-export {
-    sendInt,
-    sendFloat,
-    sendString,
-    sendARequest,
-    submit,
-    load,
-    toggle
-}
+export {setMinMode};
+export {submit, load}
 
-
-
-async function refreshAllData(url, prefix) {
-    let hwData = await con.getStatus(url)
-
-    if (hwData) {
-        con.setConnected(prefix + "_connect_status", true);
-        updateUi(prefix, hwData);
-    }
-    else {
-        con.setConnected(prefix + "_connect_status", false);
-    }
-}
 
 let minMode = false;
-
-
 function setMinMode() {
     minMode = true;
 }
 
-function updateElement(id, value) {
-    if (value === undefined) {
-        con.getEl(id).innerText = "-";
-    }
-    else if (value === "") {
-        con.getEl(id).innerText = "-";
-    }
-    else {
-        con.getEl(id).innerText = value;
-    }
-}
-
 function updateUi(prefix, hwData) {
 
-    updateElement(prefix + '_request_id', hwData["request_id"]);
-    updateElement(prefix + '_request_finished', hwData["request_finished"]);
-    updateElement(prefix + '_error', hwData["error"]);
-    updateElement(prefix + '_first', hwData["motor_1_position"]);
-    updateElement(prefix + '_second', hwData["motor_2_position"]);
+    con.updateElement(prefix + '_request_id', hwData["request_id"]);
+    con.updateElement(prefix + '_request_finished', hwData["request_finished"]);
+    con.updateElement(prefix + '_error', hwData["error"]);
+    con.updateElement(prefix + '_first', hwData["motor_1_position"]);
+    con.updateElement(prefix + '_second', hwData["motor_2_position"]);
 
     if (!minMode) {
-        updateElement(prefix + '_first_position', hwData["motor_1_position"]);
-        updateElement(prefix + '_first_temperature', hwData["motor_1_temperature"]);
-        updateElement(prefix + '_first_step', hwData["motor_1_steps"]);
-        updateElement(prefix + '_first_offset', hwData["motor_1_offset"]);
-        updateElement(prefix + '_update_m1_position', hwData["motor_1_updating_position"]);
-        updateElement(prefix + '_update_m1_temperature', hwData["motor_1_updating_temperature"]);
+        con.updateElement(prefix + '_first_position', hwData["motor_1_position"]);
+        con.updateElement(prefix + '_adv_first_position', hwData["motor_1_position"]);
+        con.updateElement(prefix + '_first_temperature', hwData["motor_1_temperature"]);
+        con.updateElement(prefix + '_first_step', hwData["motor_1_steps"]);
+        con.updateElement(prefix + '_first_offset', hwData["motor_1_offset"]);
+        con.updateElement(prefix + '_update_m1_position', hwData["motor_1_updating_position"]);
+        con.updateElement(prefix + '_update_m1_temperature', hwData["motor_1_updating_temperature"]);
 
-        updateElement(prefix + '_second_position', hwData["motor_2_position"]);
-        updateElement(prefix + '_second_temperature', hwData["motor_2_temperature"]);
-        updateElement(prefix + '_second_step', hwData["motor_2_steps"]);
-        updateElement(prefix + '_second_offset', hwData["motor_2_offset"]);
-        updateElement(prefix + '_update_m2_position', hwData["motor_2_updating_position"]);
-        updateElement(prefix + '_update_m2_temperature', hwData["motor_2_updating_temperature"]);
+        con.updateElement(prefix + '_second_position', hwData["motor_2_position"]);
+        con.updateElement(prefix + '_second_temperature', hwData["motor_2_temperature"]);
+        con.updateElement(prefix + '_second_step', hwData["motor_2_steps"]);
+        con.updateElement(prefix + '_second_offset', hwData["motor_2_offset"]);
+        con.updateElement(prefix + '_update_m2_position', hwData["motor_2_updating_position"]);
+        con.updateElement(prefix + '_update_m2_temperature', hwData["motor_2_updating_temperature"]);
 
-        updateElement(prefix + '_debug_rs232', hwData["debug_rs232"]);
-        updateElement(prefix + '_debug_aml', hwData["debug_aml"]);
-        updateElement(prefix + '_debug_broker', hwData["debug_broker"]);
-        con.getEl(prefix + "_update_m1_position_request").checked = hwData["motor_1_updating_position"];
-        con.getEl(prefix + "_update_m1_temperature_request").checked = hwData["motor_1_updating_temperature"];
-        con.getEl(prefix + "_debug_rs232").checked = hwData["debug_rs232"];
-        con.getEl(prefix + "_debug_aml").checked = hwData["debug_aml"];
-        con.getEl(prefix + "_debug_broker").checked = hwData["debug_broker"];
+        con.updateElement(prefix + '_debug_rs232', hwData["debug_rs232"]);
+        con.updateElement(prefix + '_debug_aml', hwData["debug_aml"]);
+        con.updateElement(prefix + '_debug_broker', hwData["debug_broker"]);
+
+        con.getEl(prefix + "_update_m1_position_click").checked = hwData["motor_1_updating_position"];
+        con.getEl(prefix + "_update_m1_temperature_click").checked = hwData["motor_1_updating_temperature"];
+        con.getEl(prefix + "_debug_rs232_click").checked = hwData["debug_rs232"];
+        con.getEl(prefix + "_debug_aml_click").checked = hwData["debug_aml"];
+        con.getEl(prefix + "_debug_broker_click").checked = hwData["debug_broker"];
     }
 
 }
 
-
-async function toggle(url, prefix, id, requestKey) {
-    let value = con.getEl(prefix + "_" +id + "_request").checked;
-    let request = {};
-    request[requestKey] = value;
-    await sendRequest(url,prefix, prefix +"_" +id + "_spinner", request);
-}
-
-async function sendRequest(url, prefix, spinner, request ) {
-    let hwData = await con.sendRequestAndSpin(url, request, spinner);
-    updateUi(prefix, hwData);
-}
+con.configureUiCallBack(updateUi);
 
 function submit(url, prefix, id) {
     let pos1 = parseFloat(con.getEl(prefix + "_first_request").value);
     let pos2 = parseFloat(con.getEl(prefix + "_second_request").value);
 
     if (isNaN(pos1)|| isNaN(pos2)) {
-        alert('invalid input');
+        con.showFailureModal('Invalid input. Numbers are expected for motor positions.');
         return;
     }
 
     let request = {};
     request["set_m1_target_position"] = pos1;
     request["set_m2_target_position"] = pos2;
-    sendRequest(url, prefix, prefix + "_" + id + "_spinner", request);
+    con.sendRequestAndSpin(url, prefix, id, request);
 }
 
 function load(prefix, firstPos, secondPos){
@@ -114,37 +70,3 @@ function load(prefix, firstPos, secondPos){
 
 
 
-function sendARequest(url, prefix, id ,request) {
-    let jsonRequest =  JSON.parse(request);
-    sendRequest(url,prefix, prefix +"_" +id + "_spinner", jsonRequest);
-}
-
-function sendInt(url,prefix, id, requestKey) {
-    let value = parseInt(con.getEl(prefix + "_" +id + "_request").value);
-    if (!Number.isInteger(value)) {
-        alert("This is not a valid integer number");
-        return;
-    }
-    let request = {};
-    request[requestKey] = value;
-    sendRequest(url,prefix, prefix +"_" +id + "_spinner", request);
-}
-
-function sendString(url,prefix, id, requestKey) {
-    let value = con.getEl(prefix + "_" +id + "_request").value;
-    if (value === "Choose...") { return; }
-    let request = {};
-    request[requestKey] = value;
-    sendRequest(url,prefix, prefix +"_" +id + "_spinner", request);
-}
-
-function sendFloat(url,prefix, id, requestKey) {
-    let value = parseFloat(con.getEl(prefix + "_" +id + "_request").value);
-    if (isNaN(value)) {
-        alert("This is not a valid floating point number");
-        return;
-    }
-    let request = {};
-    request[requestKey] = value;
-    sendRequest(url,prefix, prefix +"_" +id + "_spinner", request);
-}
