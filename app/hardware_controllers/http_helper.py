8a90afe06fbbb1b202ed9b2aa7252d65d5dc185d
micjo <5049483+micjo@users.noreply.github.com>
2021-07-20 16:40:08 +0200

    added timeouts for http requests

--- app/hardware_controllers/http_helper.py
+++ app/hardware_controllers/http_helper.py
@@ -6,30 +6,36 @@ session = tornado.httpclient.AsyncHTTPClient()
 
 
 async def get_text_with_response_code(url):
-    http_response = await session.fetch(url, raise_error=False)
+    http_request = tornado.httpclient.HTTPRequest(url=url, connect_timeout=2.0, request_timeout=5.0)
+    http_response = await session.fetch(http_request, raise_error=False)
     text = tornado.escape.to_unicode(http_response.body)
     return http_response.code, text
 
 
 async def get_json_with_response_code(url):
-    http_response = await session.fetch(url, raise_error=False)
+    http_request = tornado.httpclient.HTTPRequest(url=url, connect_timeout=2.0, request_timeout=5.0)
+    http_response = await session.fetch(http_request, raise_error=False)
     json_data = tornado.escape.json_decode(http_response.body)
     return http_response.code, json_data
 
 
 async def get_text(url):
-    http_response = await session.fetch(url)
+    http_request = tornado.httpclient.HTTPRequest(url=url, connect_timeout=2.0, request_timeout=5.0)
+    http_response = await session.fetch(http_request)
     text = tornado.escape.to_unicode(http_response.body)
     return text
 
 
 async def get_json(url):
-    http_response = await session.fetch(url)
+    http_request = tornado.httpclient.HTTPRequest(url=url, connect_timeout=2.0, request_timeout=5.0)
+    http_response = await session.fetch(http_request)
     json_data = tornado.escape.json_decode(http_response.body)
     return json_data
 
 
 async def post_dictionary(url, data):
     body = json.dumps(data)
-    http_response = await session.fetch(url, method='POST', body=body, raise_error=False)
+    http_request = tornado.httpclient.HTTPRequest(url=url, connect_timeout=2.0, request_timeout=5.0, method='POST',
+                                                  body=body)
+    http_response = await session.fetch(http_request, raise_error=False)
     return http_response.code, http_response.body
