fa6d987e4cc99b27a54ad1be70eb190b08a7638f
jorden95 <jorden95@imec.be>
2021-09-24 16:49:17 +0200

    hw_control for caen uses other naming. Added runner for running locally

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export faker=false
 export env_state="prod"
 
 source venv/bin/activate
-uvicorn --reload --factory hive:create_app --root-path /hive --log-level warning
+uvicorn --factory hive:create_app --root-path /hive --log-level warning
