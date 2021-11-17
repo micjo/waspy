291b2a4e8e9bae25a8fa4120d5ecc0780b624488
jorden95 <michiel.jordens@imec.be>
2021-11-17 14:48:34 +0100

    fixed logging call and systemd call

--- app/http_routes/systemd_routes.py
+++ app/http_routes/systemd_routes.py
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.get("/api/rbs/logs")
 async def get_rbs_logs():
-    completed = subprocess.run(["/bin/journalctl -q --no-pager --since='1 day ago' > /tmp/logs.txt"], shell=True)
+    completed = subprocess.run(["/bin/journalctl --since='1 day ago' | grep -v nginx > /tmp/logs.txt"], shell=True)
     return FileResponse("/tmp/logs.txt")
 
 
@@ -16,9 +16,7 @@ async def hw_control(start: bool):
     # For these commands to work, you have to make sure that these commands can be run without having to provide a
     # password. Look for 'visudo allow command' in a search engine for more information
     if start:
-        # subprocess.run(["sudo /bin/systemctl start aml_x_y aml_det_theta aml_phi_zeta motrona"], shell=True)
-        subprocess.run(["sudo /bin/systemctl start smb"], shell=True)
+        subprocess.run(["/bin/systemctl start aml_x_y aml_det_theta aml_phi_zeta motrona"], shell=True)
     else:
-        # subprocess.run(["sudo /bin/systemctl stop aml_x_y aml_det_theta aml_phi_zeta motrona"], shell=True)
-        subprocess.run(["sudo /bin/systemctl stop smb"], shell=True)
+        subprocess.run(["/bin/systemctl stop aml_x_y aml_det_theta aml_phi_zeta motrona"], shell=True)
 
