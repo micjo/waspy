3606bb70721a147cfb605507374fcfb9b436df5f
jorden95 <michiel.jordens@imec.be>
2022-04-05 17:51:23 +0200

    refactoring job runner to a flat folder structure. now using the sql backend for trending and logbook. Added support for trending to file, stdout or logbook sql

---
+++ projects/service_mill/service_mill/erd_data_serializer.py
@@ -0,0 +1,105 @@
+import json
+import logging
+import traceback
+from datetime import datetime
+from pathlib import Path
+from shutil import copy2, move
+import copy
+import pandas as pd
+from threading import Lock
+
+from rbs_entities import DoublePath
+
+
+def _try_copy(source, destination):
+    logging.info("copying {source} to {destination}".format(source=source, destination=destination))
+    try:
+        Path.mkdir(destination.parent, exist_ok=True)
+        copy2(source, destination)
+    except:
+        logging.error(traceback.format_exc())
+
+
+class ErdDataSerializer:
+    data_dir: DoublePath
+    base_folder: Path
+    sub_folder: Path
+
+    def __init__(self, data_dir: DoublePath):
+        self.data_dir = data_dir
+        self.sub_folder = Path("")
+        self._make_folders()
+        self._lock = Lock()
+        self._abort = False
+
+    def abort(self):
+        with self._lock:
+            self._abort = True
+
+    def resume(self):
+        with self._lock:
+            self._abort = False
+
+    def aborted(self):
+        with self._lock:
+            return copy.deepcopy(self._abort)
+
+    def _make_folders(self):
+        Path.mkdir(self.data_dir.local, parents=True, exist_ok=True)
+        Path.mkdir(self.data_dir.remote, parents=True, exist_ok=True)
+
+    def clear_sub_folder(self):
+        self.sub_folder = Path("")
+
+    def set_base_folder(self, base_folder: str):
+        self.base_folder = Path(base_folder)
+        Path.mkdir(self.data_dir.local / self.base_folder, exist_ok=True)
+        Path.mkdir(self.data_dir.remote / self.base_folder, exist_ok=True)
+        subdir = "old_" + datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+        self.move_files(self.data_dir.local, subdir)
+        self.move_files(self.data_dir.remote, subdir)
+
+    def move_files(self, base, subdir):
+        files_to_move = [x for x in (base / self.base_folder).iterdir() if not x.stem.startswith("old_")]
+        if files_to_move:
+            full_subdir = base / self.base_folder / subdir
+            logging.info("Existing files found, moving them to: '" + str(full_subdir) + "'.")
+            Path.mkdir(full_subdir, exist_ok=True)
+            for file in files_to_move:
+                move(file, full_subdir)
+
+    def save_trends(self, file_stem: str, trends: dict):
+        file_stem = "trends_{}.txt".format(file_stem)
+        local = self.data_dir.local / self._get_folder() / file_stem
+        remote = self.data_dir.remote / self._get_folder() / file_stem
+        df = pd.DataFrame(trends)
+
+        with open(local, 'w+') as f:
+            f.write(df.to_csv(index=False))
+        _try_copy(local, remote)
+
+    def save_rqm(self, rqm: dict):
+        file_stem = "active_rqm.txt"
+        local = self.data_dir.local / self._get_folder() / file_stem
+        remote = self.data_dir.remote / self._get_folder() / file_stem
+        with open(local, 'w+') as f:
+            f.write("Running RQM:\n")
+            f.write(json.dumps(rqm, indent=4, default=str))
+        _try_copy(local, remote)
+
+    def set_sub_folder(self, sub_folder: str):
+        self.sub_folder = Path(sub_folder)
+        Path.mkdir(self.data_dir.remote / self.base_folder / self.sub_folder, exist_ok=True)
+
+    def _get_folder(self):
+        return self.base_folder / self.sub_folder
+
+    def save_histogram(self, histogram: str, file_stem):
+        if self.aborted():
+            return
+        file_stem += ".flt"
+        local = self.data_dir.local / self._get_folder() / file_stem
+        remote = self.data_dir.remote / self._get_folder() / file_stem
+        with open(local, 'w+') as f:
+            f.write(histogram)
+        _try_copy(local, remote)
---
+++ projects/service_mill/service_mill/erd_job.py
@@ -0,0 +1,160 @@
+import logging
+import copy
+import traceback
+from datetime import datetime, timedelta
+from typing import List
+
+from pydantic import BaseModel
+
+from erd_data_serializer import ErdDataSerializer
+from erd_entities import ErdJobModel, ErdStandardRecipe, PositionCoordinates, RecipeType, ErdDepletionRecipe
+from erd_setup import ErdSetup, get_z_range
+from job import Job
+# from app.trends.trend import Trend
+from hive_exception import HiveError
+from logbook_db import LogBookDb
+
+
+class ErdRecipeStatus(BaseModel):
+    recipe_id: str
+    start_time: datetime
+    run_time: timedelta
+    measurement_time: float
+    measurement_time_target: float
+
+
+empty_erd_recipe_status = ErdRecipeStatus(recipe_id="", start_time=datetime.now(), run_time=0,
+                                          measurement_time=0, measurement_time_target=0)
+
+
+class ErdJob(Job):
+    _data_serializer: ErdDataSerializer
+    _erd_setup: ErdSetup
+    _job_model : ErdJobModel
+    _did_error: bool
+    _error_message: str
+    _active_recipe: ErdRecipeStatus
+    _finished_recipes: List[ErdRecipeStatus]
+    _aborted: bool
+    _log_book_db: LogBookDb
+
+    def __init__(self, job_model: ErdJobModel, erd_setup: ErdSetup, data_serializer: ErdDataSerializer, log_book_db: LogBookDb):
+        self._erd_setup = erd_setup
+        self._data_serializer = data_serializer
+        self._job_model = job_model
+        self._did_error = False
+        self._error_message = "No Error"
+        self._run_time = timedelta(0)
+        self._active_recipe = empty_erd_recipe_status
+        self._finished_recipes = []
+        self._aborted = False
+        self._log_book_db = log_book_db
+
+    def execute(self):
+        self._data_serializer.set_base_folder(self._job_model.rqm_number)
+        self._erd_setup.reupload_config()
+        start_time=datetime.now()
+
+        logging.info("[RQM ERD] RQM Start: '" + str(self._job_model) + "'")
+        for recipe in self._job_model.recipes:
+            if self._aborted:
+                break
+            try:
+                self._run_recipe(recipe)
+            except Exception as e:
+                logging.error("[ERD] Recipe: {" + str(recipe) + "}\nfailed with message: " + str(e))
+                self._did_error = True
+                self._error_message = str(e)
+                logging.error(traceback.format_exc())
+                self._finished_recipes = []
+                break
+            self._finish_recipe()
+
+        end_time = datetime.now()
+
+        trends = self._log_b
+        logging.info("REPLACE TRENDING !!!")
+        # for trend in self._trends:
+        #     trend_values = trend.get_values(start_time, end_time, timedelta(seconds=1))
+        #     self._data_serializer.save_trends(trend.get_file_stem(), trend_values)
+
+        self._data_serializer.save_rqm(self.get_status())
+        self._erd_setup.resume()
+        self._data_serializer.resume()
+
+    def get_status(self):
+        self._active_recipe.run_time = datetime.now() - self._active_recipe.start_time
+        self._active_recipe.measurement_time = self._erd_setup.get_measurement_time()
+
+        finished_recipes = [recipe.dict() for recipe in self._finished_recipes]
+
+        status = {"rqm": self._job_model.dict(), "active_recipe": self._active_recipe.dict(),
+                  "finished_recipes": finished_recipes, "error_state": self._error_message}
+        return status
+
+    def abort(self):
+        logging.info("[RQM ERD] RQM abort")
+        self._aborted = True
+        self._error_message = str("Aborted RQM")
+        self._erd_setup.abort()
+        self._data_serializer.abort()
+        self._active_recipe = copy.deepcopy(empty_erd_recipe_status)
+
+    def completed(self) -> bool:
+        if self._did_error:
+            return False
+        if self._aborted:
+            return False
+        return True
+
+    def empty(self):
+        return False
+
+    def _run_recipe(self, recipe):
+        self._active_recipe.start_time = datetime.now()
+        self._active_recipe.recipe_id = recipe.file_stem
+        self._active_recipe.run_time = timedelta(0)
+        self._active_recipe.measurement_time = 0
+        self._active_recipe.measurement_time_target = recipe.measuring_time_sec
+        if recipe.type == RecipeType.standard:
+            run_standard_erd_recipe(recipe, self._erd_setup, self._data_serializer)
+        if recipe.type == RecipeType.depletion:
+            run_depletion_erd_recipe(recipe, self._erd_setup, self._data_serializer)
+
+    def _finish_recipe(self):
+        self.get_status()
+        self._finished_recipes.append(copy.deepcopy(self._active_recipe))
+        self._active_recipe = empty_erd_recipe_status
+
+
+def run_standard_erd_recipe(recipe: ErdStandardRecipe, erd_setup: ErdSetup, erd_data_serializer: ErdDataSerializer):
+    erd_setup.move(PositionCoordinates(z=recipe.z_start, theta=recipe.theta))
+    erd_setup.wait_for_arrival()
+    erd_setup.configure_acquisition(recipe.measuring_time_sec, recipe.file_stem)
+    erd_setup.start_acquisition()
+    erd_setup.wait_for_acquisition_started()
+    z_range = get_z_range(recipe.z_start, recipe.z_end, recipe.z_increment)
+    if len(z_range) == 0:
+        raise HiveError("Invalid z range")
+    wait_time = recipe.measuring_time_sec / len(z_range)
+    logging.info("testing positions: " + str(z_range) + "wait_time_sec between steps: " + str(
+        wait_time) + ", total measurement time: " + str(recipe.measuring_time_sec))
+    for z in z_range:
+        erd_setup.move(z)
+        erd_setup.wait_for(wait_time)
+
+    erd_setup.wait_for_acquisition_done()
+    erd_setup.convert_data_to_ascii()
+    erd_data_serializer.save_histogram(erd_setup.get_histogram(), recipe.file_stem)
+
+
+def run_depletion_erd_recipe(recipe: ErdDepletionRecipe, erd_setup: ErdSetup, erd_data_serializer: ErdDataSerializer):
+    # todo:
+    # z_start = 10 , z_end = 15, z_increment = 1, time = 3600, z_repeat = 2
+    # z_range : [10,11,12,13,14,15,10,11,12,13,14,15], wait for 3600/12 each time
+    print("running depletion erd recipe")
+
+
+
+
+
