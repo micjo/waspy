90f9f2038c29efd145e9e7c14e4cb8b7f02712f7
micjo <5049483+micjo@users.noreply.github.com>
2022-04-13 12:35:39 +0200

    updated endpoints for in the lab. Also trending sf6 values now

--- projects/service_mill/run_service_mill_local.sh
+++ projects/service_mill/run_service_mill_local.sh
@@ -3,7 +3,7 @@ export config_file="./home_config.toml"
 export faker=true
 export env_state="dev"
 export trend_store="/tmp/trends/"
-export logbook_url="http://localhost:8001"
+export logbook_url="http://127.0.0.1:8001"
 
 source venv/bin/activate
 #uvicorn --reload --factory hive:create_app --log-level warning --host="localhost" --port=8000
