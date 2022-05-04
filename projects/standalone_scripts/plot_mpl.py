45957f72df05253df38318585b53deb3d34bc41e
jorden95 <michiel.jordens@imec.be>
2022-05-04 12:17:14 +0200

    plotting demos with tkinter

--- projects/standalone_scripts/plot_mpl.py
+++ projects/standalone_scripts/plot_mpl.py
@@ -1,92 +1,74 @@
+# -*- coding: utf-8 -*-
+
 import sys
-import time
+from PyQt5 import QtWidgets
 
 import numpy as np
-
-from PyQt5.QtWidgets import QPushButton, QLineEdit, QHBoxLayout, QComboBox, QGridLayout, QLabel
-from matplotlib.backends.qt_compat import QtWidgets
-from matplotlib.backends.backend_qt5agg import (
-    FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 
-from lib.hardware_control.hive.hardware_control.hw_action import get_packed_histogram
-from lib.hardware_control.hive.hardware_control.rbs_entities import CaenDetectorModel
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        # self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        #
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+    def compute_initial_figure(self):
+        pass
 
 
-class ApplicationWindow(QtWidgets.QMainWindow):
+class AnimationWidget(QtWidgets.QWidget):
     def __init__(self):
-        super().__init__()
-        self._main = QtWidgets.QWidget()
-        self.setCentralWidget(self._main)
-        layout = QtWidgets.QVBoxLayout(self._main)
+        QtWidgets.QWidget.__init__(self)
 
-        self.detector = CaenDetectorModel(board="33", channel=0, identifier="", bins_min=0, bins_max=8192,
-                                          bins_width=1024)
-        title_string = "Histogram board=" + self.detector.board + ", channel=" + str(self.detector.channel)
-        self.fig = plt.figure(title_string)
-        self.title_text = plt.figtext(0.20, 0.94, title_string, size='x-large', color='blue')
+        vbox = QtWidgets.QVBoxLayout()
+        self.canvas = MyMplCanvas(self, width=5, height=4, dpi=100)
+        vbox.addWidget(self.canvas)
 
-        dynamic_canvas = FigureCanvas(self.fig)
-        layout.addWidget(dynamic_canvas)
-        layout.addWidget(NavigationToolbar(dynamic_canvas, self))
+        hbox = QtWidgets.QHBoxLayout()
+        self.start_button = QtWidgets.QPushButton("start", self)
+        self.stop_button = QtWidgets.QPushButton("stop", self)
+        self.start_button.clicked.connect(self.on_start)
+        self.stop_button.clicked.connect(self.on_stop)
+        hbox.addWidget(self.start_button)
+        hbox.addWidget(self.stop_button)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
 
-        horizontal = QGridLayout()
-        horizontal.addWidget(QLabel("Board"), 0, 0)
-        horizontal.addWidget(QLabel("Channel"), 0, 1)
-        horizontal.addWidget(QLabel("Bins min"), 0, 2)
-        horizontal.addWidget(QLabel("Bins max"), 0, 3)
-        horizontal.addWidget(QLabel("Bins width"), 0, 4)
-        horizontal.addWidget(QLineEdit(), 1, 0)
-        horizontal.addWidget(QLineEdit(), 1, 1)
-        horizontal.addWidget(QLineEdit(), 1, 2)
-        horizontal.addWidget(QLineEdit(), 1, 3)
-        horizontal.addWidget(QLineEdit(), 1, 4)
-        layout.addLayout(horizontal)
+        self.x = np.linspace(0, 5 * np.pi, 400)
+        self.p = 0.0
+        self.y = np.sin(self.x + self.p)
+        self.line, = self.canvas.axes.plot(self.x, self.y, animated=True, lw=2)
 
-        layout.addWidget(QPushButton("Update values"))
-        layout.addWidget(QPushButton("Play/Pause"))
+    def update_line(self, i):
+        self.p += 0.1
+        y = np.sin(self.x + self.p)
+        self.line.set_ydata(y)
+        return [self.line]
 
-        # self._dynamic_fig = dynamic_canvas.figure
-        # ax_size = [0.11, 0.20, 1 - 0.110,
-        #            1 - 0.27]  # [left, bottom, width, height] as fractions of figure width and height.
-        # self._dyn_axes = self._dynamic_fig.add_axes(ax_size)
+    def on_start(self):
+        self.ani = FuncAnimation(self.canvas.figure, self.update_line,
+                                 blit=True, interval=1)
 
-        self._axes = dynamic_canvas.figure.subplots()
-        self._reset_axes()
-        self._line, = self._axes.plot([], [])
-        self._timer = dynamic_canvas.new_timer(1000)
-        self._timer.add_callback(self._update_canvas)
-        self._timer.start()
-
-    def _reset_axes(self):
-        self._axes.clear()
-        self._axes.set_xlabel("Energy Level")
-        self._axes.set_ylabel("Occurrence")
-        self._axes.grid(which='both')
-        # self._axes.yaxis.set_ticks_position('left')
-        # self._axes.xaxis.set_ticks_position('bottom')
-
-    def _update_canvas(self):
-        resp_code, data = get_packed_histogram("http://localhost:20200/api/latest", self.detector)
-        # self._reset_axes()
-        self._line.set_data(range(len(data)), data)
-        self._axes.set_xlim(0, 1024)
-        self._axes.set_ylim(0, 50000)
-        self._axes.autoscale()
-        self._line.figure.canvas.draw()
+    def on_stop(self):
+        self.ani._stop()
 
 
 if __name__ == "__main__":
-    # Check whether there is already a running QApplication (e.g., if running
-    # from an IDE).
-    qapp = QtWidgets.QApplication.instance()
-    if not qapp:
-        qapp = QtWidgets.QApplication(sys.argv)
+    qApp = QtWidgets.QApplication(sys.argv)
+    aw = AnimationWidget()
+    aw.show()
+    sys.exit(qApp.exec_())
 
-    app = ApplicationWindow()
-    app.show()
-    app.activateWindow()
-    app.raise_()
-    qapp.exec()
---
+++ projects/standalone_scripts/plot_tk_histogram.py
@@ -0,0 +1,104 @@
+import tkinter
+from tkinter import ttk
+
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
+import numpy as np
+
+from hive.hardware_control.hw_action import get_packed_histogram
+from hive.hardware_control.rbs_entities import CaenDetectorModel
+
+root = tkinter.Tk()
+root.wm_title("Embedding in Tk")
+
+fig = Figure(figsize=(5, 4), dpi=100)
+t = np.arange(0, 3, .01)
+ax = fig.add_subplot()
+line, = ax.plot(t, 2 * np.sin(2 * np.pi * t))
+ax.set_xlabel("time [s]")
+ax.set_ylabel("f(t)")
+ax.set_xlim(0,1024)
+ax.set_ylim(0,5000)
+ax.set_xlabel("Energy Level")
+ax.set_ylabel("Occurrence")
+ax.grid(which='both')
+
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+canvas.draw()
+
+# pack_toolbar=False will make it easier to use a layout manager later on.
+toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
+toolbar.update()
+
+canvas.mpl_connect(
+    "key_press_event", lambda event: print(f"you pressed {event.key}"))
+canvas.mpl_connect("key_press_event", key_press_handler)
+
+button_quit = tkinter.Button(master=root, text="Quit", command=root.quit)
+
+
+def update_values():
+    detector = CaenDetectorModel(board="33", channel=0, identifier="", bins_min=0, bins_max=8192,
+                                 bins_width=1024)
+
+    resp_code, data = get_packed_histogram("http://localhost:20200/api/latest", detector)
+    line.set_data(range(len(data)), data)
+
+    # required to update canvas and attached toolbar!
+    canvas.draw()
+    root.after(1000,update_values)
+
+
+def update_frequency(new_val):
+    # retrieve frequency
+    f = float(new_val)
+
+    # update data
+    # y = 2 * np.sin(2 * np.pi * f * t)
+    # line.set_data(t, y)
+
+
+
+update_values()
+
+outer_frame = tkinter.Frame(root, borderwidth=2, relief="groove")
+outer_frame.pack(side="bottom", fill="both", expand=True, padx=20, pady=20)
+
+tkinter.Label(outer_frame, text="Child Frame 1").pack(side='left')
+tkinter.Label(outer_frame, text="Child Frame 2").pack(side='left')
+tkinter.Label(outer_frame, text="Child Frame 3").pack(fill="both", expand=True)
+
+# s1 = ttk.Separator(outer_frame, orient="horizontal")
+# s2 = ttk.Separator(outer_frame, orient="horizontal")
+#
+# f1.pack(side="top", fill="both", expand=True)
+# s1.pack(side="top", fill="x", expand=False)
+# f2.pack(side="top", fill="both", expand=True)
+# s2.pack(side="top", fill="x", expand=False)
+# f3.pack(side="top", fill="both", expand=True)
+
+
+slider_update = tkinter.Scale(root, from_=1, to=5, orient=tkinter.HORIZONTAL,
+                              command=update_frequency, label="Frequency [Hz]")
+
+# Packing order is important. Widgets are processed sequentially and if there
+# is no space left, because the window is too small, they are not displayed.
+# The canvas is rather flexible in its size, so we pack it last which makes
+# sure the UI controls are displayed as long as possible.
+button_quit.pack(side=tkinter.BOTTOM, anchor='w')
+slider_update.pack(side=tkinter.BOTTOM)
+toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+frame = tkinter.Frame(root)
+tkinter.Label(outer_frame, text="Child label 1").grid()
+
+
+
+
+
+tkinter.mainloop()
