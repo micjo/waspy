473e3b3d3fd4a5581ddddfa320c7d096dcf1e0fe
micjo <5049483+micjo@users.noreply.github.com>
2022-07-18 11:38:49 +0200

    fixed path for virtualenv

--- projects/hive/run_hive_local.sh
+++ projects/hive/run_hive_local.sh
@@ -5,6 +5,6 @@ export env_state="dev"
 export trend_store="/tmp/trends/"
 export logbook_url="http://127.0.0.1:8001"
 
-source venv/bin/activate
+source ../../venv/bin/activate
 #uvicorn --reload --factory hive:create_app --log-level warning --host="localhost" --port=8000
 uvicorn --factory src.hive.main:create_app --log-level warning --host="localhost" --port=8000 --reload
