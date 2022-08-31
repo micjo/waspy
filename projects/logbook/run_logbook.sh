95d495fa09c4619966f5555fa128f8eee72fb309
Michiel Jordens <5049483+micjo@users.noreply.github.com>
2022-08-31 14:15:03 +0200

    remove unnecessary comments

--- projects/logbook/run_logbook.sh
+++ projects/logbook/run_logbook.sh
@@ -2,5 +2,4 @@
 
 export DB_FILE="imec.db"
 source ../../venv/bin/activate
-#uvicorn --factory src.logbook.main:main --root-path /logbook --log-level warning --host="0.0.0.0" --port=8001 
 uvicorn --factory src.logbook.main:main --log-level warning --host="0.0.0.0" --port=8001  --reload
