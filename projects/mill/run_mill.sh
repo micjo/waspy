da7834995724267de60d5a9e6fc079037dae798c
micjo <5049483+micjo@users.noreply.github.com>
2022-11-07 17:39:33 +0100

    new config names + no rootpath required

--- projects/mill/run_mill.sh
+++ projects/mill/run_mill.sh
@@ -2,7 +2,7 @@
 export config_file="./lab_config.toml"
 export faker=false
 export env_state="prod"
-export logbook_url="http://127.0.0.1:8001"
+export logbook_url="https://db.capitan.imec.be"
 
 source ../../venv/bin/activate
-uvicorn --factory src.mill.main:create_app --root-path /mill --log-level warning --host="0.0.0.0" --port=8000
+uvicorn --factory src.mill.main:create_app --log-level warning --host="0.0.0.0" --port=8000
