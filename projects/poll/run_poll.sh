6a41a30d323aa8608851eeea4acb588ad9dcd4bd
micjo <5049483+micjo@users.noreply.github.com>
2023-01-06 16:10:13 +0100

    fixed poll path. no longer called trend

--- projects/poll/run_poll.sh
+++ projects/poll/run_poll.sh
@@ -5,5 +5,5 @@ source ../../venv/bin/activate
 export CONFIG_FILE="config.toml"
 export LOG_TO="logbook"
 export LOGBOOK_URL="http://172.16.3.203:8001"
-python src/trend/main.py
+python src/poll/main.py
 
